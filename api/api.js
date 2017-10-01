bfe23f018cb1c9a8bd1fc5f957d422f8c88a38cc
torresNicolasMauro <torres.nicolas.mauro@gmail.com>
2017-10-01 15:50:46 -0300

    end tutorial

--- api/api.js
+++ api/api.js
@@ -9,12 +9,12 @@ var models = require('./models')(mongoose);
 
 module.exports.createUser = function (req, res) {
     const { errors, isValid } = validateInput(req.body);
-     
+
     if (!isValid) {
         res.status(400).json(errors);
     } else {
         const _username = req.body.username;
-        const _password = bcrypt.hashSync(req.body.password,10);
+        const _password = bcrypt.hashSync(req.body.password, 10);
         const _email = req.body.email;
         const _timezone = req.body.timezone;
         var newUser = new models.Users({
@@ -23,14 +23,14 @@ module.exports.createUser = function (req, res) {
             email: _email,
             timezone: _timezone
         });
-        
-        models.Users.findOne({email : _email}, function(err0, res0){
-            if (res0){
+
+        models.Users.findOne({ email: _email }, function (err0, res0) {
+            if (res0) {
                 // el email es repitido, lo agrego a los errores, checkeo si el nombre es repitido
                 errors.email = 'email repetido';
             }
-            models.Users.findOne({username : _username}, function(err1,res1){
-                if (res1){
+            models.Users.findOne({ username: _username }, function (err1, res1) {
+                if (res1) {
                     //nombre de usuario repetido, lo agrego a los errores
                     errors.username = 'nombre de usuario repetido';
                 }
@@ -46,46 +46,46 @@ module.exports.createUser = function (req, res) {
     };
 }
 
-module.exports.checkUniqueUserOrEmail = function(req,res){
+module.exports.checkUniqueUserOrEmail = function (req, res) {
     const _identifier = req.params.identifier;
     debugger;
-    models.Users.find({$or:[ {'username': _identifier}, {'email': _identifier}]} , function(err,user) {
+    models.Users.find({ $or: [{ 'username': _identifier }, { 'email': _identifier }] }, function (err, user) {
         var errors = {};
         debugger;
-        if (user.length > 0) {     
+        if (user.length > 0) {
             errors.repeated = 'ya hay un usuario con dicho';
-            res.status(200).json({errors});
+            res.status(200).json({ errors });
         } else {
-        res.status(200).json({success:false});
+            res.status(200).json({ success: false });
         }
     });
 }
 
-module.exports.authenticateUser = function (req,res){
+module.exports.authenticateUser = function (req, res) {
     debugger;
-    const { identifier, password } = req.body;   
-    models.Users.findOne({$or:[ {'username': identifier}, {'email': identifier}]}, function(err,user){
+    const { identifier, password } = req.body;
+    models.Users.findOne({ $or: [{ 'username': identifier }, { 'email': identifier }] }, function (err, user) {
         var errors = {};
         debugger;
-        if (user) {     
-            if (bcrypt.compareSync(password, user.get('password'))){
-               const token = jwt.sign({
-                   id: user.get('id'),
-                   username:user.get('username')
-               },config.jwtSecret); 
-               res.json({token});
+        if (user) {
+            if (bcrypt.compareSync(password, user.get('password'))) {
+                const token = jwt.sign({
+                    id: user.get('id'),
+                    username: user.get('username')
+                }, config.jwtSecret);
+                res.json({ token });
             } else {
-                res.status(401).json({errors: {form : 'invalid credentials'}});
+                res.status(401).json({ errors: { form: 'invalid credentials' } });
             }
         } else {
-            res.status(401).json({errors: {form : 'invalid credentials'}});
+            res.status(401).json({ errors: { form: 'invalid credentials' } });
         }
     })
 }
 
-module.exports.createEvent = function(req,res){
+module.exports.createEvent = function (req, res) {
     // MOCK data
-    res.status(201).json({ success : true });
+    res.status(201).json({ success: true });
 }
 
 module.exports.authenticate = function (req, res, next) {
@@ -103,15 +103,16 @@ module.exports.authenticate = function (req, res, next) {
                 res.status(401).json({ error: 'failed to authenticated' });
             } else {
                 debugger;
-                
-                models.Users.findOne({ username : decoded.username },function (err,user) {
+
+                models.Users.findOne({ username: decoded.username }, function (err, user) {
                     debugger;
-                    if (!user){
-                        res.status(404).json({error : 'no such user'});
+                    if (!user) {
+                        res.status(404).json({ error: 'no such user' });
+                    } else {
+                        req.currentUser = { username: user.username, timezone: user.timezone };
+                        next();
                     }
-                    req.currentUser = {username: user.username, timezone: user.timezone};
-                    next();
-                });    
+                });
             }
         });
     } else {
@@ -123,7 +124,7 @@ module.exports.authenticate = function (req, res, next) {
 
 
 
-function validateInput (data) {
+function validateInput(data) {
     let errors = {};
 
     if (validator.isEmpty(data.username)) {
